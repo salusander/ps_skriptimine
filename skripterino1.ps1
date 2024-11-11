43ef581471943d907b58c1f22bf0ba3c8739f349
Sander Salu <sander.salu.002@voco.ee>
2024-11-11 14:34:07 +0200

    Prac 1

---
+++ skripterino1.ps1
@@ -0,0 +1,9 @@
+﻿$arrray = @(1,2,3)
+$arrray2 = @(4,5,6)
+$arrray3 = @()
+
+$arrray3 = =+ $arrray[0]+$arrray2[0]
+$arrray3 = =+ $arrray[1]+$arrray2[1]
+$arrray3 = =+ $arrray[2]+$arrray2[2]
+
+$arrray3
