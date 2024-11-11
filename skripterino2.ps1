dd6ab7e5552ce6ef456a9845488c4da41f925017
Sander Salu <sander.salu.002@voco.ee>
2024-11-11 15:28:03 +0200

    Prac 2 - Hash Tables

---
+++ skripterino2.ps1
@@ -0,0 +1,4 @@
+﻿$tabel01 = @{Name="John","Joe","Mary"; DaysWorked=12,20,18}
+$tabel02 = @{Name="John","Joe","Mary"; SalaryPerDay=100,120,150}
+$tabel03 = @{Name=$tabel01.Name; Salary=($tabel01.DaysWorked[0]*$tabel02.SalaryPerDay[0]),($tabel01.DaysWorked[1]*$tabel02.SalaryPerDay[1]),($tabel01.DaysWorked[2]*$tabel02.SalaryPerDay[2])}
+$tabel03
