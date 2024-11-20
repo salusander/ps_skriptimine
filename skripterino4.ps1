4a3bbf82546cedf174ab53ed42d1f0db1ab56276
Sander Salu <sander.salu.002@voco.ee>
2024-11-20 10:49:04 +0200

    Prac 4 - II part with for loop and input control

---
+++ skripterino4.ps1
@@ -0,0 +1,14 @@
+﻿$countries = @('India', 'Australia', 'China')
+$capitals = @('New Delhi', 'Canberra', 'Beijing')
+
+Write-Host "Select country:" -ForegroundColor Yellow
+for($i = 0; $i -lt $countries.Count; $i++){
+    Write-Host ($i + 1)":"$countries[$i] -ForegroundColor Cyan
+}
+
+$selectedCountry = Read-Host "Please select country"
+if($selectedCountry -ge 1 -and $selectedCountry -le $countries.Count){
+    Write-Host "$($countries[$selectedCountry-1])'s capital is $($capitals[$selectedCountry-1])" -ForegroundColor Green
+} else {
+    Write-Host "Use only 1-3 numbers" -ForegroundColor Red
+}
