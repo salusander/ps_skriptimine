324bc72d8efaeb5c3f94941fea4a7e20602b1ccc
Sander Salu <sander.salu.002@voco.ee>
2024-11-20 11:37:56 +0200

    Prac 6

---
+++ skripterino6.ps1
@@ -0,0 +1,25 @@
+﻿$data = Import-Csv "C:\Users\Salu\ps_skriptimine\data.csv"
+
+$schoolData = @()
+foreach($personData in $data){
+
+    $personData.Age = [int]$personData.Age
+    $school = ""
+
+    if($personData.Age -ge 4 -and $personData.Age -le 10){
+        $school = "Junior"
+    } elseif($personData.Age -ge 11 -and $personData.Age -le 17){
+        $school = "Senior"
+    }
+
+    $schoolData += @{'Name'=$personData.Name; 'School'=$school}
+}
+
+# convert data to string
+$csvContent = @()
+$csvContent += "Name,School"  # header
+foreach ($row in $schoolData) {
+    $csvContent += "$($row.Name),$($row.School)"
+}
+
+$csvContent | Set-Content -Path "C:\Users\Salu\ps_skriptimine\school_data.csv"
