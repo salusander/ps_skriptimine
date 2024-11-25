6f6a28b355c685fb1482e6c232fe9fc92ac00c14
Sander Salu <sander.salu.002@voco.ee>
2024-11-25 13:54:19 +0200

    Prac 10 - until count Get-Process output

---
+++ skripterino10.ps1
@@ -0,0 +1,7 @@
+﻿$step = 0
+do {
+    Write-Host 'Notepad is running'
+    $step++
+    Start-Sleep -Seconds 1
+} until((Get-Process | Where{$_.ProcessName -eq 'notepad'}).Count -eq 0)
+Write-Host $step
