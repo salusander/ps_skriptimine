f911e3beefbb9ff28b18d1ecd605730b3eabe276
Sander Salu <sander.salu.002@voco.ee>
2024-11-25 12:12:56 +0200

    Prac 7 - Get-Random & For loop

---
+++ skripterino7.ps1
@@ -0,0 +1,9 @@
+﻿$groups = @("Red", "Yellow", "Blue")
+$studentGroups = @()
+
+for($i = 1; $i -le 20; $i++){
+    $group = $groups | Get-Random
+    $studentGroups += [PSCustomObject]@{"RollNumber"=$i; "Group"=$group}
+}
+
+$studentGroups
